0ab7bfdc66ab872a5cd39a4e44d087fe5582d19f
rahul557-shah <81374679+rahul557-shah@users.noreply.github.com>
2021-04-10 10:25:44 +0545

    Update Question40.java

--- Day4/Question40.java
+++ Day4/Question40.java
@@ -10,13 +10,13 @@
 import java.util.*;
 
 public class Question40 {
-    public boolean sequence(String s1, String s2) {
+    public boolean sequence(String s, String t) {
         int index = 0;
-        for (int i = 0; i < s2.length(); i++) {
-            if (s1.charAt(index) == s2.charAt(i)) {
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(index) == t.charAt(i)) {
                 index++;
             }
-            if (index >= s1.length()) {
+            if (index >= s.length()) {
                 return true;
             }
         }
@@ -26,9 +26,9 @@ public class Question40 {
     public static void main(String[] args) {
         System.out.println("Enter two strings string: ");
         Scanner sc = new Scanner(System.in);
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
+        String s = sc.nextLine();
+        String t = sc.nextLine();
         Question40 obj = new Question40();
-        System.out.println(obj.sequence(s1, s2));
+        System.out.println(obj.sequence(s, t));
     }
 }
